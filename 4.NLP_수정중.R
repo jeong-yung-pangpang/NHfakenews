4b7d8ccdd786b2109b68efdf5a7b7783371b8ca2
hiseoung <74298527+hiseoung@users.noreply.github.com>
2020-12-14 11:19:57 +0900

    Update 4.NLP_수정중.R

--- 4.NLP_수정중.R
+++ 4.NLP_수정중.R
@@ -117,7 +117,7 @@ dtmTfIdf <- DocumentTermMatrix( x = corpus, control = list( removeNumbers = TRUE
 dtmTfIdf
 as.matrix(dtmTfIdf[1:5, 1:500])
 
-# 차원축소 : 희소성(sparsity)이 0.95가 넘는 열 삭제
+# 차원축소 : 희소성(sparsity)이 0.99가 넘는 열 삭제
 spdtm = removeSparseTerms(dtmTfIdf, 0.99)
 spdtm
 as.matrix(spdtm[1:5, 1:20])
@@ -219,154 +219,30 @@ perf_eval(cm)
 confusionMatrix(pred_majority,test$label)
 
 ==========================================================================================================
+test$label <- predictnaivesubject
 
-#데이터 스플릿
-news_train1 <- text[1:32312,]
-news_test1 <- text[32313:46161,]
+label_0 <- test %>%
+  filter(label == 0)
 
-#사전 훈련된 word2vec사용
-word2vec_model <- read.word2vec(file = "ko.w2v", nomalize = TRUE)
+label_1 <- test %>%
+  filter(label == 1)
 
-#조정 가능한 변수, 
-max_words <- 15000 #구성할 word_index개수
-maxlen <- 32 # 단어의 최대길이
+label_0 <- label_0[1:200,]
+label_1 <- label_1[1:200,]
 
-#tokenizer를 이용해서 단어를 정수인코딩 
-tokenizer <- text_tokenizer(num_words = max_words) %>%
-  fit_text_tokenizer(text)
-sequences <- texts_to_sequences(tokenizer, nouns3)
-word_index1 <- as.matrix(word_index)
-word_index <- tokenizer$word_index
+tsne_data <- rbind(label_0, label_1)
 
+tsne_label <- tsne_data[,120]
+tsne_data <- tsne_data[,-120]
+tsne_data <- as.matrix(tsne_data)
 
-#데이터 패딩, 단어의 길이를 맞춤
-data = pad_sequences(sequences, maxlen = maxlen)
+table(tsne_label)
+head(tsne_data)
 
-#데이터 스플릿층
-train_matrix = data[1:nrow(news_train1),]
-test_matrix = data[(nrow(news_train1)+1):nrow(data),]
-
-labels = news_train_2$info
-
-training_samples = nrow(train_matrix)*0.90
-validation_samples = nrow(train_matrix)*0.10
-
-indices = sample(1:nrow(train_matrix))
-training_indices = indices[1:training_samples]
-validation_indices = indices[(training_samples + 1): (training_samples + validation_samples)]
-
-x_train = train_matrix[training_indices,]
-y_train = labels[training_indices]
-
-x_val = train_matrix[validation_indices,]
-y_val = labels[validation_indices]
-
-dim(x_train)
-table(y_train)
-
-#단어 임베딩
-embeddings_index = new.env(hash = TRUE, parent = emptyenv())
-news_embedding_dim = 300
-news_embedding_matrix = array(0, c(max_words, news_embedding_dim))
-
-#가져는 왔는데 사용 상황자체가 다르기 때문에 큰 의미가 없는듯
-for (word in names(word_index)){
-  index <- word_index[[word]]
-  if(index < max_words){
-    news_embedding_vector = embeddings_index[[word]]
-    if(!is.null(news_embedding_vector))
-      news_embedding_matrix[index+1,] <- news_embedding_vector
-  }
-}
+# news 데이터를 matrix로 변환시킨 후 t-SNE 적용
+install.packages("tsne")
+library("tsne")
+news_tsne = tsne::tsne(tsne_data)
 
 
-#input
-input <- layer_input(
-  shape = list(NULL),
-  dtype = "int32",
-  name = "input"
-)
-
-
-#hidden layer
-
-embedding <- input %>% 
-  layer_embedding(input_dim = max_words, output_dim = news_embedding_dim, name = "embedding")
-
-lstm <- embedding %>% 
-  layer_lstm(units = maxlen,dropout = 0.25, recurrent_dropout = 0.25, return_sequences = FALSE, name = "lstm")
-
-dense <- lstm %>%
-  layer_dense(units = 128, activation = "relu", name = "dense") 
-
-predictions <- dense %>% 
-  layer_dense(units = 1, activation = "sigmoid", name = "predictions")
-
-
-
-#모델 구축
-model <- keras_model(input, predictions)
-
-# Freeze the embedding weights initially to prevent updates propgating back through and ruining our embedding
-# 이거 쓰면 정확도 안나옴, 사용 데이터랑 호환이 안되는 듯..?
-# get_layer(model, name = "embedding") %>%
-#   set_weights(list(news_embedding1_matrix)) %>%
-#   freeze_weights()
-
-
-# Compile
-
-model %>% compile(
-  optimizer = optimizer_adam(),
-  loss = "binary_crossentropy",
-  metrics = "binary_accuracy"
-)
-
-print(model)
-
-#모델 훈련(20분 ~ 30분)
-history <- model %>% fit(
-  x_train,
-  y_train,
-  batch_size = 2048,
-  validation_data = list(x_val, y_val),
-  epochs = 35,
-  view_metrics = FALSE,
-  verbose = 0
-)
-
-#결과 출력
-print(history)
-plot(history)
-
-#예측값
-predictions <- predict(model, test_matrix)
-predictions <- ifelse(predictions >= 0.5, 1, 0)
-
-#성능 평가
-perf_eval <- function(cm){
-  # true positive rate
-  TPR = Recall = cm[2,2]/sum(cm[2,])
-  # precision
-  Precision = cm[2,2]/sum(cm[,2])
-  # true negative rate
-  TNR = cm[1,1]/sum(cm[1,])
-  # accuracy
-  ACC = sum(diag(cm)) / sum(cm)
-  # balance corrected accuracy (geometric mean)
-  BCR = sqrt(TPR*TNR)
-  # f1 measure
-  F1 = 2 * Recall * Precision / (Recall + Precision)
-  
-  re <- data.frame(TPR = TPR,
-                   Precision = Precision,
-                   TNR = TNR,
-                   ACC = ACC,
-                   BCR = BCR,
-                   F1 = F1)
-  return(re)
-}
-
-cm <- table(pred=predictions, actual=news_test$label)
-perf_eval(cm)
 
